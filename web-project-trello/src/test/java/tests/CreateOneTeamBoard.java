24167d72c65d23230769bbc8b5752e678147f588
AlexanderBuyanov1972 <AlexanderBuyanov1972@users.noreply.github.com>
2019-05-30 12:47:24 +0300

    2019/05/30

--- web-project-trello/src/test/java/tests/CreateOneTeamBoard.java
+++ web-project-trello/src/test/java/tests/CreateOneTeamBoard.java
@@ -3,7 +3,7 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CreateTeam extends TestBase {
+public class CreateOneTeamBoard extends TestBase {
     @Test
     public void createTeamTest() throws InterruptedException {
         app.getHeaderHelper().clickOnPlusButtonOnHeader();
